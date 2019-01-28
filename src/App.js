91cafdc781b055f3e47c9b7c4af59f0ef8ff534d
ilya <kryazhev98@icloud.com>
2019-01-28 16:36:23 +0400

    add slider

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Grid from 'react-bootstrap/lib/Grid';
+
+import styled from 'styled-components';
+
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+
+
+class App extends Component {
+  render() {
+    return (
+           <div className="App" >
+            <Header/>
+            <Menu/>
+            <Main/>
+           </div>
+    );
+  }
+}
+
+
+
+
+export default App;
